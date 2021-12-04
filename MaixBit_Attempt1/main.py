3fd134d13eba91179281644d55d6894c895b9a43
f-caro <fa.caro.aguilera@gmail.com>
2021-12-04 13:41:43 -0300

    testing fw with LVGL and kmodel V3 support

--- MaixBit_Attempt1/main.py
+++ MaixBit_Attempt1/main.py
@@ -1,24 +1,84 @@
+# 
+# from fpioa_manager import *
+# import os, Maix, lcd, image
+# from Maix import FPIOA, GPIO
+# 
+# test_pin=16
+# fm.fpioa.set_function(test_pin,FPIOA.GPIO7)
+# test_gpio=GPIO(GPIO.GPIO7,GPIO.IN)
+# lcd.init(color=(255,0,0))
+# lcd.draw_string(lcd.width()//2-68,lcd.height()//2-4, "Welcome to MaixPy", lcd.WHITE, lcd.RED)
+# if test_gpio.value() == 0:
+#     print('PIN 16 pulled down, enter test mode')
+#     lcd.clear(lcd.PINK)
+#     lcd.draw_string(lcd.width()//2-68,lcd.height()//2-4, "Test Mode, wait ...", lcd.WHITE, lcd.PINK)
+#     import sensor
+#     import image
+#     sensor.reset()
+#     sensor.set_pixformat(sensor.RGB565)
+#     sensor.set_framesize(sensor.QVGA)
+#     sensor.run(1)
+#     lcd.freq(16000000)
+#     while True:
+#         img=sensor.snapshot()
+#         lcd.display(img)
 
-from fpioa_manager import *
-import os, Maix, lcd, image
-from Maix import FPIOA, GPIO
+import sensor, image, lcd, time
+import KPU as kpu
+import gc, sys
 
-test_pin=16
-fm.fpioa.set_function(test_pin,FPIOA.GPIO7)
-test_gpio=GPIO(GPIO.GPIO7,GPIO.IN)
-lcd.init(color=(255,0,0))
-lcd.draw_string(lcd.width()//2-68,lcd.height()//2-4, "Welcome to MaixPy", lcd.WHITE, lcd.RED)
-if test_gpio.value() == 0:
-    print('PIN 16 pulled down, enter test mode')
-    lcd.clear(lcd.PINK)
-    lcd.draw_string(lcd.width()//2-68,lcd.height()//2-4, "Test Mode, wait ...", lcd.WHITE, lcd.PINK)
-    import sensor
-    import image
-    sensor.reset()
+def lcd_show_except(e):
+    import uio
+    err_str = uio.StringIO()
+    sys.print_exception(e, err_str)
+    err_str = err_str.getvalue()
+    img = image.Image(size=(224,224))
+    img.draw_string(0, 10, err_str, scale=1, color=(0xff,0x00,0x00))
+    lcd.display(img)
+
+def main(model_addr=0x300000, lcd_rotation=0, sensor_hmirror=False, sensor_vflip=False):
+    try:
+        sensor.reset()
+    except Exception as e:
+        raise Exception("sensor reset fail, please check hardware connection, or hardware damaged! err: {}".format(e))
     sensor.set_pixformat(sensor.RGB565)
     sensor.set_framesize(sensor.QVGA)
+    sensor.set_hmirror(sensor_hmirror)
+    sensor.set_vflip(sensor_vflip)
     sensor.run(1)
-    lcd.freq(16000000)
-    while True:
-        img=sensor.snapshot()
-        lcd.display(img)
+
+    lcd.init(type=1)
+    lcd.rotation(lcd_rotation)
+    lcd.clear(lcd.WHITE)
+
+    anchors = (1.889, 2.5245, 2.9465, 3.94056, 3.99987, 5.3658, 5.155437, 6.92275, 6.718375, 9.01025)
+    try:
+        task = None
+        task = kpu.load(model_addr)
+        kpu.init_yolo2(task, 0.5, 0.3, 5, anchors) # threshold:[0,1], nms_value: [0, 1]
+        while(True):
+            img = sensor.snapshot()
+            t = time.ticks_ms()
+            objects = kpu.run_yolo2(task, img)
+            t = time.ticks_ms() - t
+            if objects:
+                for obj in objects:
+                    img.draw_rectangle(obj.rect())
+            img.draw_string(0, 200, "t:%dms" %(t), scale=2)
+            lcd.display(img)
+    except Exception as e:
+        raise e
+    finally:
+        if not task is None:
+            kpu.deinit(task)
+
+
+if __name__ == "__main__":
+    try:
+        main( model_addr=0x300000, lcd_rotation=0, sensor_hmirror=False, sensor_vflip=False)
+        # main(model_addr="/sd/m.kmodel")
+    except Exception as e:
+        sys.print_exception(e)
+        lcd_show_except(e)
+    finally:
+        gc.collect()
